553e311f13e58c793ffc91013ca118211eed120b
TheRogueNinja <shreyassawant018@gmail.com>
2023-07-15 16:44:10 -0400

    add and remove cubes

--- src/components/Cube.js
+++ src/components/Cube.js
@@ -1,16 +1,52 @@
 import { useBox } from "@react-three/cannon"
 import * as textures from "../images/textures"
+import { useStore } from "../hooks/useStore"
 
-export const Cube = ({pos, texture}) => {
-    const [ref] = useBox(()=>({
-        type: 'Static',
-        pos
-    }))
+export const Cube = ({position, texture}) => {
+    const [ref] = useBox(() => ({
+		type: 'Static',
+		position
+	}))
     const activeTexture = textures[texture+'Texture']
+    const [addCube, removeCube] = useStore((state) => [state.addCube, state.removeCube])
     return(
-        <mesh ref={ref}>
+        <mesh
+            onClick = {(e)=>{
+                e.stopPropagation()
+                const clickedFace = Math.floor(e.faceIndex/2)
+                const {x,y,z} = ref.current.position
+                console.log(clickedFace)
+                if(e.altKey){
+                    removeCube(x,y,z)
+                }
+                else{
+                    switch (clickedFace) {
+                        case 0:
+                            addCube(x+1, y ,z)
+                            break;
+                        case 1:
+                            addCube(x-1, y ,z)
+                            break;
+                        case 2:
+                            addCube(x, y+1 ,z)
+                            break;
+                        case 3:
+                            addCube(x, y-1 ,z)
+                            break;
+                        case 4:
+                            addCube(x, y ,z+1)
+                            break;
+                        case 5:
+                            addCube(x, y ,z-1)
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }} 
+            ref={ref}>
             <boxBufferGeometry attach="geometry" />
-            <meshStandardMaterial map={activeTexture} attach='material' />
+            <meshStandardMaterial map={activeTexture} attach="material" />
         </mesh>
     )
 }
--- src/components/Cubes.js
+++ src/components/Cubes.js
@@ -1,12 +1,13 @@
-import { useStore } from "../hooks/useStore";
-import { Cube } from "./Cube";
-export const Cubes = () =>{
-    const [cubes] = useStore((state)=>[
-        state.cubes
-    ])
-    return cubes.map(({key, pos, texture}) =>{
-        return(
-            <Cube key={key} pos={pos} texture={texture} />
-        )
-    })
+import { useStore } from '../hooks/useStore'
+import { Cube } from './Cube'
+
+export const Cubes = () => {
+	const [cubes] = useStore((state) => [
+		state.cubes
+	])
+	return cubes.map(({ key, pos, texture }) => {
+		return (
+			<Cube key={key} position={pos} texture={texture} />
+		)
+	})
 }
